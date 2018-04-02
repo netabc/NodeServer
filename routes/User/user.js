97802372bf3fb72a905c3f99e8fb416f6be5521d
netabc <imgpng@163.com>
2018-04-02 12:18:21 +0800

    创建Nodejs服务项目

---
+++ routes/User/user.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/User/login', function(req, res, next) {
+  var user_name=req.query.username;  
+  var password=req.query.password; 
+  console.log("User name = "+user_name+", password is "+password);  
+  res.end("yes");  
+});
+
+router.post('/User/login', function(req, res, next) {
+  var user_name=req.body.username;  
+  var password=req.body.password; 
+  console.log("User name = "+user_name+", password is "+password);
+  console.log(JSON.string(req.body));
+  res.end("yes");  
+});
+
+module.exports = router;
+
