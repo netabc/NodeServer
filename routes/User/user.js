0aa7f49e2955fef89003321071de026031e48ae3
netabc <imgpng@163.com>
2018-04-02 17:36:23 +0800

    添加服务器

--- routes/User/user.js
+++ routes/User/user.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var router = express.Router();
-
+router.use(bodyParser.json());
+//router.use(bodyParser.urlencoded({ extended: false }));
 /* GET users listing. */
 router.get('/User/login', function(req, res, next) {
   var user_name=req.query.username;  
   var password=req.query.password; 
   console.log("User name = "+user_name+", password is "+password);  
-  res.end("yes");  
+  res.end("yes");
 });
 
-router.post('/User/login', function(req, res, next) {
-  var user_name=req.body.username;  
-  var password=req.body.password; 
+router.post('/User/login', function(req, res,next) {
+  var user_name=req.body.username;
+  var password=req.body.password;
   console.log("User name = "+user_name+", password is "+password);
   console.log(JSON.string(req.body));
-  res.end("yes");  
+  res.end("yes");
 });
-
 module.exports = router;
-
